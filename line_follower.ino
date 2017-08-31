a1d4e1911085cd0a3008476d59d72d14a3aa7670
gregjhansell97 <gregjhansell@gmail.com>
2017-08-31 14:58:01 -0400

    I just made the basic skeleton file for the line follower. We'll have to add more subroutines as we go along.

---
+++ line_follower.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  //initial start-up items
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //should have subroutines branching off from here
+}

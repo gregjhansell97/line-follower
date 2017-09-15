72d7a5d70e8436218ad74bce9a62f2bcf563d2ff
gregjhansell97 <gregjhansell@gmail.com>
2017-09-15 14:34:11 -0400

    initial direction driven car

--- line_follower.ino
+++ line_follower.ino
@@ -1,12 +1,14 @@
-#define MAX_RIGHT_MOTOR 255.0
-#define MIN_RIGHT_MOTOR 127.0 //need the point zero so its treated as a float
+#define MAX_RIGHT_MOTOR 178.0//about straight
+#define MAX_LEFT_MOTOR 140.0
+#define MIN_RIGHT_MOTOR 0.0 //need the point zero so its treated as a float
+#define MIN_LEFT_MOTOR 0.0
+#define MAX_PW 70
 
+const int right_motor_forward = 3;
+const int right_motor_reverse = 9;
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;  
-
-const int right_motor_forward = 6;
-const int right_motor_reverse = 3;
+const int left_motor_forward = 6;
+const int left_motor_reverse = 5;
 
 void setup() {
   // put your setup code here, to run once: 
@@ -14,22 +16,70 @@ void setup() {
   pinMode(right_motor_forward, OUTPUT);
   pinMode(right_motor_reverse, OUTPUT);
   //initial start-up items
-
 }
 
+
+
 void loop() {
-  right_motor(-100);
-  //analogWrite(right_motor_forward, 255);
+  delay(10);
+  set_direction(0);
+  delay(10);
+  hard_turn(1);
+}
+
+void straight(){
+  right_motor(MAX_PW);
+  left_motor(MAX_PW);
+}
+
+// o is straight
+//5 is all the way right
+//-5 is all the way left 
+void set_direction(int change){
+  if(change < -5) change = -5;
+  if(change > 5) change = 5;
+
+  //right handling:
+  int right_pw = MAX_PW - (MAX_PW/5.0)*change;
+  if(change < 0) right_pw = MAX_PW;
+
+  //left handling:
+  int left_pw  = MAX_PW + (MAX_PW/5.0)*change;
+  if(change > 0) left_pw = MAX_PW; 
+
+  right_motor(right_pw);
+  left_motor(left_pw);
+}
+
+//-1 is left
+//1 is right
+//ignored otherwise
+void hard_turn(int change){
+  if(change == 1){ //sharp right
+    right_motor(-MAX_PW/4.0);
+    left_motor(MAX_PW/4.0);  
+  }else if(change == -1){
+    right_motor(MAX_PW/4.0);
+    left_motor(-MAX_PW/4.0);
+  }
+}
+
+void stop(){
+  right_motor(0);
+  left_motor(0);
 }
 
 void right_motor(int duty){
   drive_motor(right_motor_forward, right_motor_reverse, MAX_RIGHT_MOTOR, MIN_RIGHT_MOTOR, duty);
 }
 
+void left_motor(int duty){
+  drive_motor(left_motor_forward, left_motor_reverse, MAX_LEFT_MOTOR, MIN_LEFT_MOTOR, duty);
+}
+
 void drive_motor(const int forward, const int reverse, const int max_m, const int min_m, int duty){
   if(duty > 100) duty = 100;
   if(duty < -100) duty = -100;
-  delay(10);
   if(duty > 0){ //forwards
     analogWrite(forward, ((max_m - min_m)/100.0)*duty + min_m);
     analogWrite(reverse, 0);
